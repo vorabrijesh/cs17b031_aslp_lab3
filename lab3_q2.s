521089939e6eb6042879840e5af2a0cdb21b13c9
brijesh vora <cs17b031@iittp.ac.in>
2019-02-22 16:59:56 +0530

    Update lab3_q2.s

--- lab3_q2.s
+++ lab3_q2.s
@@ -1,10 +1,10 @@
 main:
-li $s0, 1
-li $s1, 2
-li $s2, 1
-li $s3, 4
-mul $t0, $s0, $s3
-mul $t0, $t0, $s3
-mul $t1, $s1, $s3
-sub $t0, $t0, $t1
-add $t0, $t0, $s2
+li $s0, 1   # a=1
+li $s1, 2   # b=2  
+li $s2, 1   # c=1
+li $s3, 4   # x=4
+mul $t0, $s0, $s3     #t0=a*x
+mul $t0, $t0, $s3     #t0=t0*x=a*x*x
+mul $t1, $s1, $s3     #t1=b*x
+add $t0, $t0, $t1     #t0=t0+t1 = a*x*x+ b*x 
+add $t0, $t0, $s2     #t0=t0+s2= a*x*x+b*x+c
